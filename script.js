d76576a1e09a651f25bba3cbe75115477e3c55b2
Léo Maugeri <leomaugeri@hotmail.fr>
2025-05-01 19:24:51 +0200

    updated portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('contactForm');
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      alert('Message envoyé !');
-      form.reset();
-    });
-  });
-  
